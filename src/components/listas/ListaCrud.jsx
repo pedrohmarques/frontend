e8383d1952852c0ce4fb56981b86af694d7e2037
Pedro Almeida <pedrim6661@hotmail.com>
2019-03-10 22:47:38 -0300

    Começo da lista e backend integrado

--- src/components/listas/ListaCrud.jsx
+++ src/components/listas/ListaCrud.jsx
@@ -1,16 +1,33 @@
 import React, { Component } from 'react'
 import Main from '../template/Main'
+import axios from 'axios'
+
+import NewList from './NewList'
 
 const headerProps = {
     icon: 'list-alt',
     title: 'Lista de Compras'
 }
 
+const listGroup = 'http://localhost:8080/users'
+const initialState = {
+    list: []
+}
+
 export default class ListaCrud extends Component {
+    state = {...initialState}
+    
+    componentWillMount(){
+        axios(`${listGroup}/9/groups`).then(resp=>{
+            this.setState({ list: resp.data })
+        })
+    }
+
     render(){
+        console.log(this.state.list)
         return(
             <Main {...headerProps}>
-                Cadastro da ListaS
+                <NewList></NewList>
             </Main>
         )
     }
---
+++ src/components/listas/NewList.jsx
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+
+export default class NewList extends Component{
+
+    render(){
+        return(
+            <button className="btn btn-light btn-lg btn-block">
+            <i className="fa fa-plus"> Nova Lista</i>
+        </button>
+        )
+    }
+}
